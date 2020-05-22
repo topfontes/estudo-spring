a348c85ffb9f171c0803352f86ae6b69c91f489b
Marcos Augusto <topfontes@gmail.com>
2020-05-22 16:27:18 -0300

    criando primeiro serviço rest

---
+++ src/main/java/com/soluction/resources/CategoriaResource.java
@@ -0,0 +1,16 @@
+package com.soluction.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource {
+
+	@RequestMapping(method = RequestMethod.GET, value = "olamundo")
+	public String olaMundo() {
+		return "Olá mundo";
+	}
+
+}
