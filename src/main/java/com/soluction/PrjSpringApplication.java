5fd236f4e6293da916b457d5dec946d8f9c47550
Marcos Augusto <topfontes@gmail.com>
2020-05-22 14:51:50 -0300

    Criação do projeto

---
+++ src/main/java/com/soluction/PrjSpringApplication.java
@@ -0,0 +1,13 @@
+package com.soluction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soluction/PrjSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soluction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
