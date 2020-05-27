06dd82d2c76bf96b118c69a3d7e1876508699768
Marcos Augusto <topfontes@gmail.com>
2020-05-27 13:05:20 -0300

    git teste

--- src/main/java/com/soluction/resources/CategoriaResource.java
+++ src/main/java/com/soluction/resources/CategoriaResource.java
@@ -1,16 +1,34 @@
 package com.soluction.resources;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.soluction.model.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
-	@RequestMapping(method = RequestMethod.GET, value = "olamundo")
+	@RequestMapping(method = RequestMethod.GET, value = "/olamundo")
 	public String olaMundo() {
 		return "Olá mundo";
 	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/getcategoria")
+	public List<Categoria> getcategoria() {
+		
+		List<Categoria> lista = new ArrayList<Categoria>();
+		lista.add(new Categoria(1,"Informatica"));
+		lista.add(new Categoria(2,"Escritório"));
+		return lista;
+		
+	}
+		
+	
 
 }
